ac81bc393dd7ca672406211ccc313fce5449099e
Victor <victorcorreaa95@gmail.com>
2024-02-06 19:17:19 -0300

    feat: Add component Log to show the turns

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
   function handleSelectSquare() {
     setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    setGameTurns(prevTurns => {
+      const updatedTurns = []
+    });
   }
   return (
     <main>
@@ -18,7 +23,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
